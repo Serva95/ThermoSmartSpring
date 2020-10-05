e838e766dc7ff2ef38cd64cb6fa948dba6c5975b
Serva <43950391+Serva95@users.noreply.github.com>
2020-10-05 16:22:18 +0200

    created REST method in new controllers

--- src/main/java/it/srv/ThermoSmartSpring/OrariOnOffController.java
+++ src/main/java/it/srv/ThermoSmartSpring/OrariOnOffController.java
@@ -1,4 +1,33 @@
 package it.srv.ThermoSmartSpring;
 
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
 public class OrariOnOffController {
+
+    @GetMapping("/orarionoffs")
+    public ModelAndView orariOnOffs(ModelAndView mav) {
+        return mav;
+    }
+
+    @GetMapping("/orarionoffs/{id}")
+    public ModelAndView viewOrariOnOff(ModelAndView mav, @PathVariable int id) {
+        return mav;
+    }
+
+    @PostMapping("/orarionoffs/{id}")
+    public ModelAndView createOrariOnOff(ModelAndView mav, @PathVariable int id) {
+        return mav;
+    }
+
+    @PutMapping("/orarionoffs/{id}")
+    public ModelAndView updateOrariOnOff(ModelAndView mav, @PathVariable int id) {
+        return mav;
+    }
+
+    @DeleteMapping("/orarionoffs/{id}")
+    public ModelAndView deleteOrariOnOff(ModelAndView mav, @PathVariable int id) {
+        return mav;
+    }
 }
--- src/main/java/it/srv/ThermoSmartSpring/RoomController.java
+++ src/main/java/it/srv/ThermoSmartSpring/RoomController.java
@@ -1,4 +1,33 @@
 package it.srv.ThermoSmartSpring;
 
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
 public class RoomController {
+
+    @GetMapping("/rooms")
+    public ModelAndView Rooms(ModelAndView mav) {
+        return mav;
+    }
+
+    @GetMapping("/rooms/{id}")
+    public ModelAndView viewRoom(ModelAndView mav, @PathVariable int id) {
+        return mav;
+    }
+
+    @PostMapping("/rooms/{id}")
+    public ModelAndView createRoom(ModelAndView mav, @PathVariable int id) {
+        return mav;
+    }
+
+    @PutMapping("/rooms/{id}")
+    public ModelAndView updateRoom(ModelAndView mav, @PathVariable int id) {
+        return mav;
+    }
+
+    @DeleteMapping("/rooms/{id}")
+    public ModelAndView deleteRoom(ModelAndView mav, @PathVariable int id) {
+        return mav;
+    }
 }
