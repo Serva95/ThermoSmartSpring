25241b8d7ecae0b8362ef6134014ce6dd4c6b815
Serva <43950391+Serva95@users.noreply.github.com>
2020-10-19 00:56:37 +0200

    ended html page newOrariOnOff.html + its controller's get method

--- src/main/java/it/srv/ThermoSmartSpring/OrariOnOffController.java
+++ src/main/java/it/srv/ThermoSmartSpring/OrariOnOffController.java
@@ -1,12 +1,17 @@
 package it.srv.ThermoSmartSpring;
 
 import it.srv.ThermoSmartSpring.dao.OrariOnOffDAO;
+import it.srv.ThermoSmartSpring.dto.OrariOnOffDTO;
 import it.srv.ThermoSmartSpring.model.OrariOnOff;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.time.DayOfWeek;
+import java.time.format.TextStyle;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 @RestController
 public class OrariOnOffController {
@@ -23,27 +28,33 @@ public class OrariOnOffController {
 
     @GetMapping("/rooms/{id}/orarionoff/new")
     public ModelAndView newOrariOnOffs(ModelAndView mav, @PathVariable int id) {
-        List<OrariOnOff> orariOnOffs = orariOnOffDAO.getByRoomId(id);
-        mav.addObject("orarionoffs", orariOnOffs);
-        mav.setViewName("viewOrariOnOff");
+        OrariOnOffDTO orariOnOffDTO = new OrariOnOffDTO();
+        ArrayList<String> days = new ArrayList<>();
+        for (int i = 0; i < 7; i++) {
+            orariOnOffDTO.add(new OrariOnOff());
+            days.add(DayOfWeek.of(i+1).getDisplayName(TextStyle.FULL, Locale.ITALY));
+        }
+        mav.addObject("days", days);
+        mav.addObject("orarionoffDTO", orariOnOffDTO);
+        mav.setViewName("newOrariOnOff");
         return mav;
     }
 
     @GetMapping("/rooms/{id}/orarionoff/{giorno}")
-    public ModelAndView viewOrariOnOff(ModelAndView mav, @PathVariable int id) {
+    public ModelAndView viewOrariOnOff(ModelAndView mav, @PathVariable int id, @PathVariable int giorno) {
         mav.setViewName("editOrariOnOff");
         return mav;
     }
 
-    @PostMapping("/rooms/{id}/orarionoff/{id}")
+    @PostMapping("/rooms/{id}/orarionoff")
     public ModelAndView createOrariOnOff(ModelAndView mav, @PathVariable int id) {
         mav.addObject("message", "Stato modificato con successo.");
-        mav.setViewName("redirect:/rooms/"+id);
+        mav.setViewName("redirect:/rooms/"+id+"/orarionoff");
         return mav;
     }
 
-    @PutMapping("/rooms/{id}/orarionoff/{id}")
-    public ModelAndView updateOrariOnOff(ModelAndView mav, @PathVariable long id) {
+    @PutMapping("/rooms/{id}/orarionoff/{giorno}")
+    public ModelAndView updateOrariOnOff(ModelAndView mav, @PathVariable long id, @PathVariable int giorno) {
         mav.addObject("message", "Stato modificato con successo.");
         mav.setViewName("redirect:/rooms/"+id);
         return mav;
