4c93ca72ea150c9cef5ed196b99a846353b88cca
Serva <43950391+Serva95@users.noreply.github.com>
2020-10-20 01:36:50 +0200

    createOrariOnOff edit page completed + general fix

--- src/main/java/it/srv/ThermoSmartSpring/dto/OrariOnOffDTO.java
+++ src/main/java/it/srv/ThermoSmartSpring/dto/OrariOnOffDTO.java
@@ -1,5 +1,6 @@
 package it.srv.ThermoSmartSpring.dto;
 
+import it.srv.ThermoSmartSpring.model.OrariOnOff;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,8 +15,35 @@ public class OrariOnOffDTO {
         this.orariOnOffs = new ArrayList<>();
     }
 
-    public void add(OrariOnOffStringDTO orariOnOff, short giorno) {
+    public void add(final OrariOnOffStringDTO orariOnOff, final short giorno) {
         orariOnOff.setGiorno(giorno);
         this.orariOnOffs.add(orariOnOff);
+    }
+
+    /**
+     * Converte una lista di 7 elementi di tipo OrariOnOff, quindi con tipo LocalTime per gli orari,
+     * in tipo OrariOnOffStringDTO con tipo String per gli orari, in modo da facilitare la creazione
+     * e la successiva lettura nelle pagine html che usano tale lista
+     * @param orariOnOffs di tipo List&lt;OrariOnOff&gt; è la lista di 7 elementi da convertire
+     */
+    public void orariToStringDTO (final List<OrariOnOff> orariOnOffs){
+        ArrayList<OrariOnOffStringDTO> orari = new ArrayList<>();
+        for (OrariOnOff e : orariOnOffs) {
+            OrariOnOffStringDTO stringDTO = new OrariOnOffStringDTO();
+            stringDTO.setId(e.getId());
+            stringDTO.setGiorno(e.getGiorno());
+            stringDTO.setOrarioAccensioneA(e.getOrarioAccensioneA().toString());
+            stringDTO.setOrarioSpegnimentoA(e.getOrarioSpegnimentoA().toString());
+            if (e.getOrarioAccensioneB() != null) {
+                stringDTO.setOrarioAccensioneB(e.getOrarioAccensioneB().toString());
+                stringDTO.setOrarioSpegnimentoB(e.getOrarioSpegnimentoB().toString());
+            }
+            if (e.getOrarioAccensioneC() != null) {
+                stringDTO.setOrarioAccensioneC(e.getOrarioAccensioneC().toString());
+                stringDTO.setOrarioSpegnimentoC(e.getOrarioSpegnimentoC().toString());
+            }
+            orari.add(stringDTO);
+        }
+        this.orariOnOffs = orari;
     }
 }
