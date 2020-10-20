fa29d4f68b8651bcb8529094d12939603c0b7ae0
Serva <43950391+Serva95@users.noreply.github.com>
2020-10-20 18:54:04 +0200

    OrariOnOff all method and pages completed + small fix

--- src/main/java/it/srv/ThermoSmartSpring/services/OrariOnOffService.java
+++ src/main/java/it/srv/ThermoSmartSpring/services/OrariOnOffService.java
@@ -16,6 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalTime;
 import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 @Service
@@ -31,8 +32,32 @@ public class OrariOnOffService {
             throws BlankFieldsException, InvalidFieldException, ObjectAlreadyExistException {
         Room room = roomDAO.get(id);
         ArrayList<OrariOnOff> toSave = new ArrayList<>();
+        orariObjectConverter(orari, room, toSave, null);
+        List<OrariOnOff> orariOnOffs = orariOnOffDAO.getByRoomId(id);
+        if(orariOnOffs.iterator().hasNext()) throw new ObjectAlreadyExistException("Esistono già degli orari per questa stanza, non inserirne altri");
+        orariOnOffDAO.saveAll(toSave);
+    }
+
+    public void updateOrari(OrariOnOffDTO orari, int id) throws BlankFieldsException, InvalidFieldException{
+        Room room = roomDAO.get(id);
+        ArrayList<OrariOnOff> toSave = new ArrayList<>();
+        List<OrariOnOff> orariOnOffs = orariOnOffDAO.getByRoomId(id);
+        orariObjectConverter(orari, room, toSave, orariOnOffs);
+        orariOnOffDAO.saveAll(toSave);
+    }
+
+    private void orariObjectConverter(OrariOnOffDTO orari, Room room, ArrayList<OrariOnOff> toSave, List<OrariOnOff> orariOnOffs) throws BlankFieldsException, InvalidFieldException {
+        final String msg1 = "L'orario di accensione non può essere successivo a quello di spegnimento, riprova.";
+        final String msg2 = "L'orario di accensione di una fascia deve essere successivo a quello di spegnimento della fascia precedente, riprova.";
+        Iterator<OrariOnOff> iter = null;
+        if (orariOnOffs!= null ){
+            iter = orariOnOffs.iterator();
+        }
         for (OrariOnOffStringDTO e : orari.getOrariOnOffs()) {
             OrariOnOff tmp = new OrariOnOff();
+            if (orariOnOffs!= null ){
+                tmp.setId(iter.next().getId());
+            }
             tmp.setGiorno((short) (e.getGiorno()+1));
             tmp.setRoom(room);
             try {
@@ -43,21 +68,21 @@ public class OrariOnOffService {
                     throw new BlankFieldsException("Completa tutti i campi e riprova");
                 tmp.setOrarioSpegnimentoA(LocalTime.parse(e.getOrarioSpegnimentoA()));
                 if(tmp.getOrarioAccensioneA().isAfter(tmp.getOrarioSpegnimentoA()))
-                    throw new InvalidFieldException("L'orario di accensione non può essere successivo a quello di spegnimento, riprova.");
+                    throw new InvalidFieldException(msg1);
                 if (!(e.getOrarioAccensioneB().equals("") || e.getOrarioSpegnimentoB().equals(""))) {
                     tmp.setOrarioAccensioneB(LocalTime.parse(e.getOrarioAccensioneB()));
                     tmp.setOrarioSpegnimentoB(LocalTime.parse(e.getOrarioSpegnimentoB()));
                     if(tmp.getOrarioAccensioneB().isAfter(tmp.getOrarioSpegnimentoB()))
-                        throw new InvalidFieldException("L'orario di accensione non può essere successivo a quello di spegnimento, riprova.");
+                        throw new InvalidFieldException(msg1);
                     if(tmp.getOrarioSpegnimentoA().isAfter(tmp.getOrarioAccensioneB()))
-                        throw new InvalidFieldException("L'orario di accensione di una fascia deve essere successivo a quello di spegnimento della fascia precedente, riprova.");
+                        throw new InvalidFieldException(msg2);
                     if (!(e.getOrarioAccensioneC().equals("") || e.getOrarioSpegnimentoC().equals(""))) {
                         tmp.setOrarioAccensioneC(LocalTime.parse(e.getOrarioAccensioneC()));
                         tmp.setOrarioSpegnimentoC(LocalTime.parse(e.getOrarioSpegnimentoC()));
                         if(tmp.getOrarioAccensioneC().isAfter(tmp.getOrarioSpegnimentoC()))
-                            throw new InvalidFieldException("L'orario di accensione non può essere successivo a quello di spegnimento, riprova.");
+                            throw new InvalidFieldException(msg1);
                         if(tmp.getOrarioSpegnimentoB().isAfter(tmp.getOrarioAccensioneC()))
-                            throw new InvalidFieldException("L'orario di accensione di una fascia deve essere successivo a quello di spegnimento della fascia precedente, riprova.");
+                            throw new InvalidFieldException(msg2);
                     }
                 }
             } catch (DateTimeParseException ex) {
@@ -65,8 +90,5 @@ public class OrariOnOffService {
             }
             toSave.add(tmp);
         }
-        List<OrariOnOff> orariOnOffs = orariOnOffDAO.getByRoomId(id);
-        if(orariOnOffs.iterator().hasNext()) throw new ObjectAlreadyExistException("Esistono già degli orari per questa stanza, non inserirne altri");
-        orariOnOffDAO.saveAll(toSave);
     }
 }
