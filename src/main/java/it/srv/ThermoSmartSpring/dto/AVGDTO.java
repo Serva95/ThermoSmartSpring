a99d1f18cef02dc2e69114f2df24493edbede8e3
Serva <43950391+Serva95@users.noreply.github.com>
2020-11-20 01:06:02 +0100

    springframework update to 2.4.0 + bug fix

--- src/main/java/it/srv/ThermoSmartSpring/dto/AVGDTO.java
+++ src/main/java/it/srv/ThermoSmartSpring/dto/AVGDTO.java
@@ -1,9 +1,8 @@
 package it.srv.ThermoSmartSpring.dto;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
 
 public interface AVGDTO {
     BigDecimal getTemp();
-    LocalDate getGiorno();
+    String getGiorno();
 }
